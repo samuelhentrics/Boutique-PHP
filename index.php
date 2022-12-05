f23ea45541928998062ef9dc3b01859cad346691
Samuel HENTRICS <samuel.hentrics@gmail.com>
2022-12-05 08:39:27 +0100

    Nav

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <? include("rsc/template/head.php"); ?>
+</head>
+<body>
+    <? include("rsc/template/nav.php"); ?>
+
+    <? include("rsc/template/footer.php"); ?>
+</body>
+</html>
---
+++ rsc/template/nav.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Accueil</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="articles.php">Articles</a>
+      </li>
+      <li class="nav-item">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Se connecter</button>
+      </li>
+    </ul>
+  </div>
+</nav>
