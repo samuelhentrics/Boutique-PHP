472b61befa1f080e2110938b53d9931ff4eddfb4
BenjaminPeyre <bpeyre@univ-pau.fr>
2022-12-26 18:06:00 +0100

    affichage Articles avec lien bd et debut d'un panier

--- articles.php
+++ articles.php
@@ -20,9 +20,76 @@
 
 <body>
     <?php include("rsc/template/nav.php"); ?>
+    <script  language="javascript">
+function getXMLHttpRequest() {
+	var xhr = null;
+	if (window.XMLHttpRequest || window.ActiveXObject) {
+		if (window.ActiveXObject) {
+			try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		} else {
+			xhr = new XMLHttpRequest(); 
+		}
+	} else {
+		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+		return null;
+	}
+	return xhr;
+}
+function ajoutCdSession(id){
+var xhr = getXMLHttpRequest();
+//Déclaration d'une fonction sur l'événement onreadystatechange qui évolue au fur et à mesure de l'appel
+xhr.onreadystatechange = function () {
+  if(this.readyState == 4) //si la requête à abouti et est terminée
+  {    
+     if(this.status == 200) // si le code de retour est 200, le serveur à répondu et envoyé une réponse
+     {
+        //Le traitement à effectuer avec la réponse : affichage via alert, passage à une fonction, mise un jour du DOM (réponse dans xhr.responseText et dans xhr.responseXML).
+        // var resultat = JSON.parse(this.responseText);
+        // document.getElementById('info').innerHTML = "Fiche de l'utilisateur : Nom : " + resultat.nom + " Prénom : " + resultat.prenom + " Tel : " + resultat.telephone; 
+     }
+     else 
+     {
+        //traitement dans le cas d'un retour en erreur code != 200 : affichage par alert, console, mise à jour du DOM. 
+        alert("Le serveur n'a pas répondu à la requête : code d'erreur : " + this.status);
+     }
+  }
+}
+xhr.open("GET","./addPanier.php?idCD=" + id,true); //Requête AJAX en mode GET sur l'url donnée.
+xhr.send(null); //Puisque c'est une requête en GET
+}
 
+</script>
     <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shloistine_bd";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM cd";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["id"]. " - Name: ". $row["titre"]. " " . $row["genre"] ." <img src=./rsc/img/".$row['url_image'].">" . "<br>";
+        print("<button onclick=ajoutCdSession(".$row["id"].")>Ajouter au panier</button>");
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+/*
     echo '<div class="container">';
 
     $listeCD = [];
@@ -30,10 +97,11 @@
 
         $monPanier = $_SESSION['monPanier'];
         // define("MON_PANIER", $monPanier);
-        function faireUnNouveauCD($genre, $titre, $auteur, $prix, $imgPochette = null)
+        function faireUnNouveauCD($id, $genre, $titre, $auteur, $prix, $imgPochette = null)
         {
 
             $cd = [
+                "id_cd" => $id,
                 "genre_cd" => $genre,
                 "titre_cd" => $titre,
                 "auteur_cd" => $auteur,
@@ -46,7 +114,7 @@
 
         function afficherListeCD($liste)
         {
-            echo '<form action="./panier.php" method="post">';
+            echo '<form action="./panier.php" method="get">';
             foreach ($liste as $l => $unCD) {
                 echo "<br>";
                 foreach ($unCD as $key => $value) {
@@ -54,7 +122,7 @@
                 }
                 //prende la cd 
                 // echo '<input type="checkbox" name="" id="">';
-                echo '<input type="number" name="" id="" placeholder="nombre de de cd" min="0" value="0"';
+                echo '<input type="number" name="'.$unCD['id_cd'].'" id="'.$unCD['id_cd'].'" placeholder="nombre de de cd" min="0" value="0"';
                 echo "<br>";
             }
             echo "<br>";
@@ -69,20 +137,26 @@
         //     $imgPochette = "";
         // } 
 
-        array_push($listeCD, faireUnNouveauCD("JPOP", "REDO", "LISA", 20));
-        array_push($listeCD, faireUnNouveauCD("RAP", "CHOP", "LISA", 20));
-        array_push($listeCD, faireUnNouveauCD("CLASIC", "AVALE", "LISA", 20));
-        array_push($listeCD, faireUnNouveauCD("OSEF", "NoName", "LISA", 20));
+        array_push($listeCD, faireUnNouveauCD(1,"JPOP", "REDO", "LISA", 20));
+        array_push($listeCD, faireUnNouveauCD(2,"RAP", "CHOP", "LISA", 20));
+        array_push($listeCD, faireUnNouveauCD(3,"CLASIC", "AVALE", "LISA", 20));
+        array_push($listeCD, faireUnNouveauCD(4,"OSEF", "NoName", "LISA", 20));
 
         afficherListeCD($listeCD);
     }
 
 
+
     echo '</div>';
-
+*/
     ?>
+  
+  <?php include("rsc/template/footer.php"); ?>
 
-    <?php include("rsc/template/footer.php"); ?>
+<script>
+var xhr = getXMLHttpRequest();
+
+</script>
 </body>
 
 </html>
--- panier.php
+++ panier.php
@@ -13,21 +13,74 @@
 </head>
 
 <body>
+<script>
+function getXMLHttpRequest() {
+	var xhr = null;
+	if (window.XMLHttpRequest || window.ActiveXObject) {
+		if (window.ActiveXObject) {
+			try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		} else {
+			xhr = new XMLHttpRequest(); 
+		}
+	} else {
+		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+		return null;
+	}
+	return xhr;
+}
+function viderPanier(){
+//Déclaration de notre objet XHR à l'aide de notre fonction déclarer plus haut
+var xhr = getXMLHttpRequest();
+//Déclaration d'une fonction sur l'événement onreadystatechange qui évolue au fur et à mesure de l'appel
+xhr.onreadystatechange = function () {
+  if(this.readyState == 4) //si la requête à abouti et est terminée
+  {    
+     if(this.status == 200) // si le code de retour est 200, le serveur à répondu et envoyé une réponse
+     {
+        //Le traitement à effectuer avec la réponse : affichage via alert, passage à une fonction, mise un jour du DOM (réponse dans xhr.responseText et dans xhr.responseXML).
+        // alert("panier vidée")
+     }
+     else 
+     {
+        //traitement dans le cas d'un retour en erreur code != 200 : affichage par alert, console, mise à jour du DOM. 
+        alert("erreur lors de la suppresion du panier");
+     }
+    location.reload()
+  }
+}
+xhr.open("GET","./supprimerPanier.php",true); //Requête AJAX en mode GET sur l'url donnée.
+xhr.send(null); //Puisque c'est une requête en GET
+}
+</script>
     <?php
 
     include("rsc/template/nav.php");
 
     echo '<div class="container">';
-    echo '<h2>Votre panier</h2>';
-
+    echo '<h2>Votre panier <button onclick="viderPanier()">Vider le panier</button></h2>';
 
     if (isset($_SESSION["monPanier"])) {
 
         function afficherElemPanier()
         {
+            // var_dump($_SESSION["monPanier"]);  
+
+
             foreach ($_SESSION["monPanier"] as $monPanier => $unElementDuPanier) {
-                print_r($unElementDuPanier);
+                echo "<br>";
+                echo '<div style="display: inline-flex;">';
+                echo '<img src="./rsc/img/'.$unElementDuPanier["url_image"].'" alt="" width="120px">';
+                echo '<h3>'.$unElementDuPanier["titre"].'</h3>';
+                echo '<h5>'.$unElementDuPanier["auteur"].'</h5>';
+                echo '<h3>'.$unElementDuPanier["prix"].'</h3>';
+                echo '</div>';
+                // echo "<br>    - Name: ". $row["titre"]. " " . $row["genre"] ." <img src=./rsc/img/".$row['url_image'].">" . "<br>";
             }
+            
         }
 
         // Cas où le panier est vide
@@ -44,6 +97,7 @@
     
 
     echo '</div>';
+
     include("rsc/template/footer.php");
 
     ?>
