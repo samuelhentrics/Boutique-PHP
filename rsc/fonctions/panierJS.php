c1620fc37a3ff0659bcb640fc6968bfbe15fe2a0
BenjaminPeyre <bpeyre@univ-pau.fr>
2022-12-29 18:13:15 +0100

    verification quantite positive

--- rsc/fonctions/panierJS.php
+++ rsc/fonctions/panierJS.php
@@ -1,6 +1,7 @@
 <html>
 <script>
   async function modifierquantitePanier(id, nombre) {
+if(nombre>0)
     try {
       const response = await fetch(`./rsc/fonctions/modifierPanier.php?idCD=${id}&quantite=${nombre}`, {
         method: 'GET'
@@ -8,13 +9,26 @@
       if (response.ok) {
         location.reload()
       } else {
-        throw new Error('Error deleting item from cart');
+        throw new Error('Erreur de modification panier');
       }
     } catch (error) {
       console.error(error);
     }
+  else {
+    try {
+        const response = await fetch(`./rsc/fonctions/supprimerPanier.php?idCD=${id}`, {
+          method: 'GET'
+        });
+        if (response.ok) {
+          location.reload()
+        } else {
+          throw new Error('Erreur de suppression panier');
+        }
+      } catch (error) {
+        console.error(error);
+      }
   }
-
+  }
 </script>
 
 </html>
